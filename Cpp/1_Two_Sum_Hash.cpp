dd257006209494173bad63cd1f37f86148112349
ronydas12345 <dasrony231@gmail.com>
2026-01-17 18:20:26 -0600

    Add C++ Hash solution for 1. Two Sum

---
+++ Cpp/1_Two_Sum_Hash.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> m; int N = nums.size();
+
+        for (int i = 0; i < N; i ++) {
+            auto j = m.find(target - nums[i]);
+            if (j != m.end()) {
+                return {i, j->second};
+            }
+
+            m.insert({nums[i], i});
+        }
+
+        return {};
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LeetCode
 My solutions for LeetCode problems
+
+1. Two Sum ([link](https://leetcode.com/problems/two-sum/description/))
+    * Hash Table
+        * [C++](https://github.com/ronydas12345/LeetCode/blob/main/Cpp/1_Two_Sum.cpp)
+        * Python
+    * Array
+        * C++
+        * Python
