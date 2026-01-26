8ba34483fd95cbeb1449f256440c3e79bdc7075a
ronydas12345 <dasrony231@gmail.com>
2026-01-26 13:18:33 -0600

    remove old unnecesary files

--- Python/1_Two_Sum_Hash.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        m = {}
-        for i, num in enumerate(nums):
-            comp = target - num
-            if comp in m:
-                return [m[comp], i]
-            m[num] = i
