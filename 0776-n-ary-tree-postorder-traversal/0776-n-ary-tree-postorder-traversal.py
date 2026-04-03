bf93e8b3ab722e547320ea3ddab84ede183df6bf
Raunak Das <dasrony231@gmail.com>
2026-04-03 14:31:10 -0500

    0776-N-ARY-TREE-POSTORDER-TRAVERSAL Stats: Time: 50 ms (81.6%), Space: 21 MB (38.65%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.py
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.py
@@ -6,16 +6,26 @@ class Node:
         self.children = children
 """
 
+# class Solution:
+#     def postorder(self, root: 'Node') -> List[int]:
+#         def rec(root):
+#             if not root: return
+#             for n in root.children:
+#                 rec(n)
+#                 res.append(n.val)
+    
+#         res = []
+#         if not root: return []
+#         rec(root)
+#         res.append(root.val)
+#         return res
+
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        def rec(root):
-            if not root: return
-            for n in root.children:
-                rec(n)
-                res.append(n.val)
-    
-        res = []
         if not root: return []
-        rec(root)
-        res.append(root.val)
-        return res
+        res, s = [], [root]
+        while s:
+            peek = s.pop()
+            res.append(peek.val)
+            s.extend(peek.children)
+        return res[::-1]
