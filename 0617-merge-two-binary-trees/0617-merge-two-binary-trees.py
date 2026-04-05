6d26721ee239570661aba7fa618253f12ec5e8d5
Raunak Das <dasrony231@gmail.com>
2026-04-05 13:45:10 -0500

    0617-MERGE-TWO-BINARY-TREES Stats: Time: 1 ms (57.04%), Space: 19.6 MB (27.45%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -4,34 +4,51 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-#         if root1 and root2:
-#             root = TreeNode(root1.val + root2.val)
-#             root.right = self.mergeTrees(root1.right, root2.right)
-#             root.left = self.mergeTrees(root1.left, root2.left)
-#             return root
-#         else:
-#             if root1 and root2: return root1
-#             elif root2: return root2
-#             elif root1: return root1
-#             else: return None
 
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1: return root2
-        s = [(root1, root2)]
-        while s:
-            a, b = s.pop()
-            if not a or not b: continue
+        if root1 and root2:
+            root = TreeNode(root1.val + root2.val)
+            root.right = self.mergeTrees(root1.right, root2.right)
+            root.left = self.mergeTrees(root1.left, root2.left)
+            return root
+        else:
+            return root1 or root2
 
-            a.val += b.val
+# class Solution:
+#     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root1: return root2
+#         s = [(root1, root2)]
+#         while s:
+#             a, b = s.pop()
+#             if not a or not b: continue
 
-            if not a.left: a.left = b.left
-            else: s.append((a.left, b.left))
+#             a.val += b.val
 
-            if not a.right: a.right = b.right
-            else: s.append((a.right, b.right))
+#             if not a.left: a.left = b.left
+#             else: s.append((a.left, b.left))
+
+#             if not a.right: a.right = b.right
+#             else: s.append((a.right, b.right))
         
-        return root1
-    
+#         return root1
+
+# class Solution:
+#     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if not root1: return root2
+#         if not root2: return root1
+
+#         q = [(root1, root2)]
+#         while q:
+#             a, b = q.pop()
+#             if not a: continue
+
+#             a.val += b.val
+
+#             if not a.left: a.left = b.left
+#             elif b.left: q.append((a.left, b.left))
+            
+#             if not a.right: a.right = b.right
+#             elif b.right: q.append((a.right, b.right))
+        
+#         return root1
