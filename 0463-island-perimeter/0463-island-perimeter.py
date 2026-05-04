9edfe9289f825976d174bd088c73ffc2dde33e81
Raunak Das <dasrony231@gmail.com>
2026-05-03 22:40:23 -0500

    0463-ISLAND-PERIMETER Stats: Time: 109 ms (31.86%), Space: 19.9 MB (41.04%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -22,10 +22,16 @@ class Solution:
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
+                    for d in dirs:
+                        i, j = r + d[0], c + d[1]
+                        if self.in_bounds(grid, i, j) or grid[i][j] == 0: res += 1
+
+                    """
+                if grid[r][c] == 1:
                     if self.in_bounds(grid, r - 1, c) or grid[r - 1][c] == 0: res += 1
                     if self.in_bounds(grid, r, c - 1) or grid[r][c - 1] == 0: res += 1
                     if self.in_bounds(grid, r + 1, c) or grid[r + 1][c] == 0: res += 1
-                    if self.in_bounds(grid, r, c + 1) or grid[r][c + 1] == 0: res += 1
+                    if self.in_bounds(grid, r, c + 1) or grid[r][c + 1] == 0: res += 1"""
         
         return res
     
